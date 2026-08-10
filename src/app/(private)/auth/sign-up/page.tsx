7c069a2aa255f1e1c259583028d994acdb071abe
oscaresteve <oscarestevea24@gmail.com>
2026-08-10 10:58:24 +0200

    feat(auth): create sign up form

--- src/app/(private)/auth/sign-up/page.tsx
+++ src/app/(private)/auth/sign-up/page.tsx
@@ -1,3 +1,9 @@
+import { SignUpForm } from "../_components/sign-up-form";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <main className="h-dvh w-full items-center flex justify-center">
+      <SignUpForm />
+    </main>
+  );
 }
