c86eac08518d1388c78f5e773e7d749838991ca1
oscaresteve <oscarestevea24@gmail.com>
2026-08-17 11:57:00 +0200

    fix: redirect to onboarding if user has not any accessible workspace

--- src/app/(private)/(global)/layout.tsx
+++ src/app/(private)/(global)/layout.tsx
@@ -1,8 +1,14 @@
+import { redirect } from "next/navigation";
 import AppHeader from "@/components/app-header";
 import { GlobalSidebar } from "@/components/global-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { hasWorkspaces } from "@/lib/api/workspaces.server";
 
 export default async function GlobalLayout({ children }: { children: React.ReactNode }) {
+  if (!(await hasWorkspaces())) {
+    redirect("/onboarding");
+  }
+
   return (
     <main className="[--header-height:calc(--spacing(12))]">
       <SidebarProvider className="flex flex-col">
