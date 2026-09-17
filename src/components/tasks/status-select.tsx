b345069a68a949b9809e3dda05150efe55b3029f
oscaresteve <oscarestevea24@gmail.com>
2026-09-17 17:50:42 +0200

    feat(status-select): make content full width

--- src/components/tasks/status-select.tsx
+++ src/components/tasks/status-select.tsx
@@ -40,7 +40,7 @@ export function StatusSelect({ id, variant = "default", value, onValueChange, cl
         <DropdownMenuTrigger id={id} render={<EnumControl option={statusOptions[value]} className={className} />} />
       )}
 
-      <DropdownMenuContent className="w-max">
+      <DropdownMenuContent>
         <DropdownMenuRadioGroup value={value} onValueChange={(next) => onValueChange(next as TaskStatus)}>
           {taskStatuses.map((status) => (
             <DropdownMenuRadioItem
